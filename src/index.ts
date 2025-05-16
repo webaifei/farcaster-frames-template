e7ee56183e3ed559f010be07d64e31b0a9c0725a
aifei.zhang <aifei.zhang@okg.com>
2025-05-16 14:07:28 +0800

    init project

--- src/index.ts
+++ src/index.ts
@@ -65,7 +65,7 @@ app.post('/', async (c) => {
   }
 })
 
-const port = 3000
+const port = process.env.PORT ? Number(process.env.PORT) : 3000
 console.log(`Server is running on port ${port.toString()}`)
 
 serve({
